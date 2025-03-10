aa528810eb4e16d88914c4daacc2182e607b527c
Den-One <deniskolchev2001@gmail.com>
2025-03-11 00:55:30 +0300

    feat: fills CardReserve with cards

--- cardreserve.cpp
+++ cardreserve.cpp
@@ -1,19 +1,63 @@
 #include "cardreserve.h"
+#include "carddataconfigreader.h"
+
+#include <QDir>
+#include <QCoreApplication>
+
+CardReserve::CardReserve()
+{
+    // find a way to config file
+    QString executablePath = QCoreApplication::applicationDirPath();
+    QDir sourceDir(executablePath);
+    sourceDir.cd("../../../"); // Move on 3 levels up
+    QString configPath = sourceDir.absolutePath() + "/CardsDataConfig.ini";
+    if (QFile::exists(configPath)) {
+        qDebug() << "Config file has found: " << configPath;
+    } else {
+        qDebug() << "File not found!";
+    }
+
+    // read card data from config
+    CardDataConfigReader cardReader(configPath);
+    auto cards = cardReader.readFromRange(4, 18);
+    for (int i = 0; i < cards.size(); ++i) {
+        if (cards[i]->type() == CardType::Landmark) {
+            m_cards.insert(cards[i], 5);
+        } else {
+            m_cards.insert(cards[i], 6);
+        }
+    }
+}
+
+CardReserve::~CardReserve()
+{
+}
 
 void CardReserve::addCard(std::shared_ptr<Card> card)
 {
-    m_cards.insert(card->title(), card);
+    auto it = m_cards.find(card);
+    if (it != m_cards.end()) {
+        m_cards.insert(card, m_cards.find(card).value() + 1);
+    } else {
+        m_cards.insert(card, 1);
+    }
 }
 
-void CardReserve::removeCard(const QString& cardName)
+uchar CardReserve::coundCards(std::shared_ptr<Card> card)
 {
-    m_cards.remove(cardName);
+    return m_cards.find(card).value();
 }
 
-std::shared_ptr<Card> CardReserve::getCard(const QString& cardName) const
+void CardReserve::removeCard(std::shared_ptr<Card> card)
 {
-    return *m_cards.find(cardName);
+    auto it = m_cards.find(card);
+    if (it.value() > 1) {
+        m_cards.insert(card, m_cards.find(card).value() + 1);
+    } else {
+        m_cards.remove(card);
+    }
 }
+
 
 void activateCard(const QString& cardName, Player& owner, Player& activePlayer, int diceRoll)
 {
--- cardreserve.h
+++ cardreserve.h
@@ -5,6 +5,30 @@
 
 #include <QMap>
 
+/*
+
+How many cards of each out there
+
+title=Wheat Field - 6
+title=Animal Farm - 6
+title=Bakery - 6
+title=Cafe - 6
+title=Shop - 6
+title=Forest - 6
+title=Stadium - 5
+title=TV Center - 5
+title=Business Center - 5
+title=Cheese Factory - 6
+title=Furniture Factory - 6
+title=Mine - 6
+title=Restaurant - 6
+title=Apple Orchard - 6
+title=Fruit Marketå - 6
+
+Feels like there is should be file called Game Rools
+
+*/
+
 class CardReserve {
 public:
     CardReserve();
@@ -13,14 +37,14 @@ public:
 
     void addCard(std::shared_ptr<Card> card);
 
-    void removeCard(const QString& cardName);
+    uchar coundCards(std::shared_ptr<Card> card);
 
-    std::shared_ptr<Card> getCard(const QString& cardName) const;
+    void removeCard(std::shared_ptr<Card> card);
 
     void activateCard(const QString& cardName, Player& owner, Player& activePlayer, int diceRoll);
 
 private:
-    QMap<QString, std::shared_ptr<Card>> m_cards;
+    QMap<std::shared_ptr<Card>, uchar> m_cards;
 };
 
 #endif // CARDRESERVE_H
